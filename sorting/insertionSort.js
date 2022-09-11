91e036768758f2f635ca81848481a6fa0917c6df
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-09-11 09:57:52 -0600

    Improve insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -7,24 +7,26 @@ const insertionSort = arr => {
 
   if (arr.length < 2) return arr
 
-  for (let x = 0; x < arr.length; x++) {
-    let item = arr[x + 1]
-    let shiftIndex = x
-    let hasShifted = false
-    while (item < arr[shiftIndex]) {
+  // easier to start at 1
+  for (let x = 1; x < arr.length; x++) {
+    let item = arr[x]
+    let position = x - 1
+
+    while (position >= 0) {
       numOps++
-      arr[shiftIndex + 1] = arr[shiftIndex]
-      shiftIndex--
-      hasShifted = true
+      if (arr[position] > item) {
+        arr[position + 1] = arr[position]
+        position--
+      } else {
+        break
+      }
     }
-    if (hasShifted) {
-      arr[shiftIndex + 1] = item
-    }
+    arr[position + 1] = item
   }
+
   console.warn('sorted array', arr)
   console.warn('number of elements', arr.length)
   console.warn('number of operations', numOps)
-  console.warn(arr[-1])
 }
 
 insertionSort(arrayOfNumbers)
